7f3943dd1ac1c8c7afd792aa6d3ccf647553c76a
lecture-bisper <bisper@outlook.kr>
2025-03-04 17:50:24 +0900

    2025-03-04 : UI 만들기 : 카카오톡 따라하기, Layout ViewGroup 클래스 : LinearLayout 사용하기, orientation, layout_weight, margin, padding, gravity, layout_gravity

---
+++ app_20250304/kakao/src/main/java/bitc/fullstack/kakao/MainActivity2.kt
@@ -0,0 +1,22 @@
+package bitc.fullstack.kakao
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.kakao.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    val binding = ActivityMain2Binding.inflate(layoutInflater)
+    setContentView(binding.root)
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
