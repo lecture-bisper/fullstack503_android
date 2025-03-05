c70dda93fa5320053e289c2716daf5ed67844b8f
lecture-bisper <bisper@outlook.kr>
2025-03-05 16:57:19 +0900

    2025-03-05 : Layout ViewGroup 클래스 : GridLayout

---
+++ app_20250305/app/src/main/java/bitc/fullstack/app_20250305/GridActivity.kt
@@ -0,0 +1,22 @@
+package bitc.fullstack.app_20250305
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.app_20250305.databinding.ActivityGridBinding
+
+class GridActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    val binding = ActivityGridBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
